a8a0262b388931978c9da54bcbf5c237e38753b9
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-03 23:18:54 +0200

    feat: Create navbar :construction:

---
+++ src/containers/Navbar/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Hexagon from "./Hexagon/Hexagon.svelte";
+</script>
+
+<style lang="scss">
+  @import "Navbar";
+</style>
+
+<div class="navbar">
+  <div class="page-title">
+    <h1> Bienvenue sur mon site Web</h1>
+    <p>Quelle section vous intéresse ?</p>
+  </div>
+  <div class="hexagon-menu">
+    <Hexagon props={{icon: 'fa-universal-access', link: 'welcome', title: 'Welcome'}}/>
+    <Hexagon props={{icon: 'fa-user', link: 'description', title: 'Description'}}/>
+  </div>
+</div>
+
