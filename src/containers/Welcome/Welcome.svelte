9f654baf2792c276fe2b649a4538e92593fc25ce
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-11 23:01:50 +0200

    feat: update with Ivan feedbacks

--- src/containers/Welcome/Welcome.svelte
+++ src/containers/Welcome/Welcome.svelte
@@ -8,11 +8,12 @@
       display: flex;
       flex-direction: column;
       align-items: center;
-      background-color: black;
       height: 100vh;
-      /*background-image: url("https://cdn.futura-sciences.com/buildsv6/images/wide1920/a/0/a/a0a74adca1_50147933_developpeur-web1.jpg");*/
-      /*filter: brightness(50%);*/
-      /*-webkit-filter: brightness(50%);*/
+      background:
+              linear-gradient(rgba(255,255,255,0.9), rgba(255,255,255,0.9)),
+              url("https://img-16.ccm2.net/tZzRh9XOkpjdTWtpDCTs1lmHDRI=/500x/df8b52ded53f4649821c3b2813839d75/ccm-faq/photo-1486312338219-ce68d2c6f44d.jfif"),
+              no-repeat;
+      background-size:cover;
   }
 </style>
 
