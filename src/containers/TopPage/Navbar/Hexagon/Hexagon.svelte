5b28e1867ba73ccfbf45ab24630586983663a2a2
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-15 19:09:29 +0200

    fix: a to span to fix empty href bug

--- src/containers/TopPage/Navbar/Hexagon/Hexagon.svelte
+++ src/containers/TopPage/Navbar/Hexagon/Hexagon.svelte
@@ -19,7 +19,7 @@
     <div></div>
     <div></div>
   </div>
-  <a use:scrollto={props.link} class="hex-content">
+  <span use:scrollto={props.link} class="hex-content">
     <span class="hex-content-inner">
         <span class="icon">
             <Icon icon={ props.icon }/>
@@ -27,5 +27,5 @@
         <span class="title">{props.title}</span>
     </span>
     <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-  </a>
+  </span>
 </div>
