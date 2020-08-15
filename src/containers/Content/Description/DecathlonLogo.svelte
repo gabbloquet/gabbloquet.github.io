508a41e3ae64efbd016e6a65507e5de7dfbea24f
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-15 14:41:00 +0200

    feat(description): add content about tech

---
+++ src/containers/Content/Description/DecathlonLogo.svelte
@@ -0,0 +1,7 @@
+<style>
+  img {
+      height: 1.5em;
+  }
+</style>
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png" alt="Decathlon">
--- src/containers/Content/Description/Description.svelte
+++ src/containers/Content/Description/Description.svelte
@@ -1,3 +1,6 @@
+<script>
+  import DecathlonLogo from "./DecathlonLogo.svelte";
+</script>
 <style lang="scss">
   @import "description";
   @import "src/styles/global";
@@ -10,7 +13,12 @@
       <p>Développeur, sportif, musicien & avide de connaissance</p>
     </div>
     <div class="right-side">
-      Petit texte
+      <p>Gabin Bloquet, 24 ans, actuellement développeur chez <DecathlonLogo/>.
+      <h2>La #Tech</h2>
+      <p>Je définirai mon profil comme étant plutôt <b>"fullstack"</b>. En quelques mots du fait des parcours universitaires français plutôt accès back j'avais tout naturellement une appétence dans ce domaine. Ma première expérience m'a permise de parfaire mes compétences dans ce domaine. Mais m'a aussi ouvert les yeux sur l'importance de la prise en compte des besoins des utilisateurs. De réfléchir sur le quoi et le comment. <i>Comment puis-je, grace à l'outil informatique, améliorer le quotidien de mes utilisateurs, leur faire gagner du temps, leur faciliter la vie ?</i></p>
+      <p>C'est donc tout naturellement que je me suis rapproché du monde du <b>front</b>, car un <b>confort d'utilisation</b> passe par une <b>interface ergonomique, accessible, simple d'utilisation et de prise en main</b>.</p>
+      <h2>Le #Sport</h2>
+      <p></p>
     </div>
   </div>
 </div>
