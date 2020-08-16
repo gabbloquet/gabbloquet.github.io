bff20ea809d6c5b20734fe10b37f85e4ab7e7256
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-16 16:33:02 +0200

    feat(experiences): add style :art:

---
+++ src/containers/Content/Experiences/Experiences.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Timeline from "./Timeline/Timeline.svelte";
+</script>
+
+<div class="experiences" id="experiences">
+  <Timeline/>
+</div>
---
+++ src/containers/Content/Experiences/Timeline/Timeline.svelte
@@ -0,0 +1,27 @@
+<style lang="scss">
+  @import "timeline";
+</style>
+
+<div class="timeline">
+  <div class="element">
+    <h2>Decathlon</h2>
+    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+  </div>
+  <div class="element">
+    <h2>Decathlon</h2>
+    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+  </div>
+  <div class="element">
+    <h2>Decathlon</h2>
+    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+  </div>
+  <div class="element">
+    <h2>Decathlon</h2>
+    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+  </div>
+  <div class="element">
+    <h2>Decathlon</h2>
+    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+  </div>
+</div>
+
