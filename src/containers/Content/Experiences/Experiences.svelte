b0141681f14a1255f4e0f1dcdcedd4f4c23bb6ad
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-08-16 18:21:17 +0200

    feat(experiences): add content and modify style with years

--- src/containers/Content/Experiences/Experiences.svelte
+++ src/containers/Content/Experiences/Experiences.svelte
@@ -2,6 +2,10 @@
   import Timeline from "./Timeline/Timeline.svelte";
 </script>
 
+<style lang="scss">
+  @import "experiences";
+</style>
+
 <div class="experiences" id="experiences">
   <Timeline/>
 </div>
--- src/containers/Content/Experiences/Timeline/Timeline.svelte
+++ src/containers/Content/Experiences/Timeline/Timeline.svelte
@@ -4,24 +4,41 @@
 
 <div class="timeline">
   <div class="element">
-    <h2>Decathlon</h2>
-    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+    <h3><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png" alt="Decathlon"/> - Développeur Full stack dans l'équipe MyBusiness - Depuis mars 2020</h3>
+    <p>J'ai rejoins l'équipe MyBusiness en tant que Développeur Fullstack avec l'optique de monter en compétences sur le rôle de Tech Lead. Nous travaillons sur le développement du nouveau Front à destination des utilisateurs magasin. Un point d'entrée unique permettant d'offrir à l'utilisateur toutes les actions utiles à la bonne gestion de son magasin, de ses investissements, de ses commandes... L'objectif est de remplacer un outil vieillissant en étant les plus user-centric possible, en offrant un confort d'utilisation et d’accessibilité au top.
+      Pour cela nous sommes inspirés de la philosophie AGILE, travaillons sur la qualité des développements et sur des technologies actuelles et ayant fait leurs preuves.</p>
+
+    <p>Parallèlement je suis Leader de la communauté Front, en quelques mots je synergise, accompagne, organise différents Events, aide les personnes qui en font la demande, donne du sens et fait en sorte que les projets de la communauté se concrétisent (Tel que le Design system par exemple).</p>
+
+    <p class="important-information">Pratiques de développement : TDD, DDD (BDD par la suite)</p>
+    <p class="important-information">Langages de développement : ReactJS, JavaScript</p>
+    <p class="important-information">Autre outils : NPM, JUnit, Cypress, GIT, IntelliJ, GitHub with actions</p>
   </div>
   <div class="element">
-    <h2>Decathlon</h2>
-    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+    <h3><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png" alt="Decathlon"/>  - Développeur Full stack dans l'équipe SignEasy - 1 an</h3>
+    <p>J'ai réalisé mon stage de fin d'études au sein de l'équipe SignEasy et ai poursuivi l'expérience pour au final y contribuer durant une année. Nous travaillions sur les différentes solutions permettantes l'impression (dans le cas d'un balisage papier) ou l'affichage (dans le cas d'un balisage électronique) des différents supports de communication que nous pouvons retrouver en magasin. (Prix du produit, avis, notation, usage...), mettant en pratique les méthodologies AGILE et Craftsmanship. </p>
+
+    <p>Parallèlement je suis core contributor de la communauté Front & membre actif de celle dédiée à l'Agile, en quelques mots je participe aux Events, aide les personnes qui en font la demande et je monte en compétences en demandant de l'aide, expose mon point de vue et est force de proposition. J'ai ensuite glissé sur le rôle de co-leader puis leader durant cette période.</p>
+
+    <p class="important-information">Pratiques de développement : TDD, BDD, DDD et Event sourcing</p>
+    <p class="important-information">Langages de développement : Java 8 - 11 (JEE), VueJS, NodeJS, JavaScript</p>
+    <p class="important-information">Autre outils : Spring, JUnit, Mockito, GIT, IntelliJ, Postman, Docker, JIRA, GitHub with actions</p>
   </div>
   <div class="element">
-    <h2>Decathlon</h2>
-    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+    <h3><img src="https://francoistison.fr/img/uploads/workexperience/pictime.png" alt="Pictime"/> - Analyste développeur dans l'équipe Nocibé - 3 mois</h3>
+    <p>J'ai réalisé mon stage (facultatif) de master 1 au sein de l'équipe chargée du développement du site Web de Nocibé. Nous étions une équipe de 10 personnes avec des profils variés.</p>
+
+    <p class="important-information">Pratiques de développement : Tests unitaires et d'intégrations</p>
+    <p class="important-information">Langages de développement : Java 8 (JEE), Bash, JSP, JavaScript, HTML, CSS</p>
+    <p class="important-information">Autre outils : Spring, Spring Batch, JUnit, Mockito, JMetter, Testcafe, IntelliJ, GIT, Xubuntu, Docker</p>
   </div>
   <div class="element">
-    <h2>Decathlon</h2>
-    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
-  </div>
-  <div class="element">
-    <h2>Decathlon</h2>
-    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+    <h3><img src="https://www.netvigie.com/bundles/netvigiewebcore/images/netvigie.png?1524002411" alt="Netvigie"/> - Développeur en recherche et développement Web - 5 mois</h3>
+    <p>Durant 5 mois, au coeur d'une équipe de développement Web, mon objectif fut de développer une partie d'une solution de monitoring (analyse du déploiement des Tags, des règles SEO, des performances...) pour les entreprises. J'y ai aussi développé un plugin chrome en JavaScript permettant de voir en live des informations liées à la page actuellement visitée.</p>
+
+    <p class="important-information">Pratiques de développement : Tout types de tests</p>
+    <p class="important-information">Langages de développement : Html, CSS, Bootstrap, Php, Symfony, Twig, JS, JQuery, AJAX</p>
+    <p class="important-information">Autre outils : Template Material-Admin, ClubHouse, Git, PhpStorm, Linux (Ubuntu), SQLYog</p>
   </div>
 </div>
 
