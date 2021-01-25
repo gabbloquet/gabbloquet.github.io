78ca5a3b991195b61177e41cac93308398fa1cb6
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-01-25 16:49:19 +0100

    feat: Update title arbo for accessibility

--- src/containers/Content/Experiences/Timeline/Timeline.svelte
+++ src/containers/Content/Experiences/Timeline/Timeline.svelte
@@ -5,7 +5,7 @@
 <div class="timeline">
   <div class="element">
     <h2><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png" alt="Decathlon"/> Développeur Full stack dans l'équipe MyBusiness</h2>
-    <h4>Depuis mars 2020</h4>
+    <h3>Depuis mars 2020</h3>
     <p>J'ai rejoint l'équipe MyBusiness en tant que Développeur Fullstack avec l'optique de monter en compétences sur le rôle de Tech Lead. </p>
     <p>Nous travaillons sur le développement du nouveau Front à destination des utilisateurs magasin. Un point d'entrée unique permettant d'offrir à l'utilisateur toutes les actions utiles à la bonne gestion de son magasin, de ses investissements, de ses commandes... L'objectif est de remplacer un outil vieillissant en étant les plus user-centric possible, en offrant un confort d'utilisation et d’accessibilité au top.
       Pour cela nous sommes inspirés de la philosophie AGILE, travaillons sur la qualité des développements et sur des technologies actuelles et ayant fait leurs preuves.</p>
@@ -20,7 +20,7 @@
   </div>
   <div class="element">
     <h2><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png" alt="Decathlon"/> Développeur Full stack dans l'équipe SignEasy</h2>
-    <h4>Une année</h4>
+    <h3>Une année</h3>
     <p>J'ai réalisé mon stage de fin d'études au sein de l'équipe SignEasy et ai poursuivi l'expérience pour au final y contribuer durant une année. Nous travaillions sur les différentes solutions permettantes l'impression (dans le cas d'un balisage papier) ou l'affichage (dans le cas d'un balisage électronique) des différents supports de communication que nous pouvons retrouver en magasin. (Prix du produit, avis, notation, usage...), mettant en pratique les méthodologies AGILE et Craftsmanship. </p>
 
     <p>Parallèlement je suis core contributor de la communauté Front & membre actif de celle dédiée à l'Agile, en quelques mots je participe aux Events, aide les personnes qui en font la demande et je monte en compétences en demandant de l'aide, expose mon point de vue et est force de proposition. J'ai ensuite glissé sur le rôle de co-leader puis leader durant cette période.</p>
@@ -32,7 +32,7 @@
   </div>
   <div class="element">
     <h2><img src="https://francoistison.fr/img/uploads/workexperience/pictime.png" alt="Pictime"/> Analyste développeur dans l'équipe Nocibé</h2>
-    <h4>Trois mois</h4>
+    <h3>Trois mois</h3>
     <p>J'ai réalisé mon stage (facultatif) de master 1 au sein de l'équipe chargée du développement du site Web de Nocibé. Nous étions une équipe de 10 personnes avec des profils variés.</p>
 
     <p class="important-information">Pratiques de développement : Tests unitaires et d'intégrations</p>
@@ -41,7 +41,7 @@
   </div>
   <div class="element">
     <h2><img src="https://www.netvigie.com/bundles/netvigiewebcore/images/netvigie.png?1524002411" alt="Netvigie"/> Développeur en recherche et développement Web</h2>
-    <h4>Cinq mois</h4>
+    <h3>Cinq mois</h3>
     <p>Durant 5 mois, au coeur d'une équipe de développement Web, mon objectif fut de développer une partie d'une solution de monitoring (analyse du déploiement des Tags, des règles SEO, des performances...) pour les entreprises. J'y ai aussi développé un plugin chrome en JavaScript permettant de voir en live des informations liées à la page actuellement visitée.</p>
 
     <p class="important-information">Pratiques de développement : Tout types de tests</p>
