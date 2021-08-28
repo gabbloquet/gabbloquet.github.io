059132a9df4b7302a327da7c0cffac1c8bf42bf1
Bloquet Gabin <gabin.bloquet@decathlon.com>
2021-08-28 17:48:13 +0200

    feat: update skills

--- src/containers/Content/Skills/Skills.svelte
+++ src/containers/Content/Skills/Skills.svelte
@@ -3,38 +3,54 @@
   @import "skills";
 </style>
 
-<section class="skills" id="skills">
-  <h1 class="skills-title">Compétences 🎯💻</h1>
-  <div class="skills-content">
-    <div class="front">
-      <img src="https://miro.medium.com/max/446/1*762I50vvsX7ddlFYdtlycw.png" alt="frontend developement (Mobile, tablette, grand écran...)"/>
-      <h2>Front</h2>
-      <h3>Langages</h3>
-      <p>HTML, CSS, JavaScript, SASS/SCSS</p>
-      <h3>Frameworks</h3>
-      <p>React.js, VueJS, Svelte</p>
-      <h3>Autres outils / librairies</h3>
-      <p>NPM, Jest, Cypress, Storybook, Testcafé, Webpack, ESlint, Prettier, Selenium...</p>
-    </div>
-    <div class="back">
-      <img src="https://www.tftus.com/blog/wp-content/uploads/2017/10/Backend-Development-banner.jpg" alt="backend developement (Secure, scalable...)"/>
-      <h2>Back</h2>
-      <h3>Langages</h3>
-      <p>Java, PHP, TypeScript, SQL</p>
-      <h3>Frameworks</h3>
-      <p>Spring, Symfony, NodeJS/Express, Nest</p>
-      <h3>Autres outils / concepts</h3>
-      <p>JGiven, Hibernate, API, REST, Serverless, Mockito</p>
-    </div>
-    <div class="others">
+<article class="skills" id="skills">
+  <header>
+    <h1 class="skills-title">Compétences 🎯💻</h1>
+  </header>
+  <article class="skills-content">
+    <header>
+      <div class="front">
+        <img src="https://miro.medium.com/max/446/1*762I50vvsX7ddlFYdtlycw.png" alt="frontend developement (Mobile, tablette, grand écran...)"/>
+        <h2>Front</h2>
+        <h3>Langages</h3>
+        <p>HTML, CSS, JavaScript, Typescript, SASS/SCSS</p>
+        <h3>Frameworks</h3>
+        <p>React.js, VueJS, Svelte</p>
+        <h3>Autres outils / librairies</h3>
+        <p>NPM, Jest, Cypress, Storybook, Testcafé, Webpack, ESlint, Prettier, Selenium...</p>
+      </div>
+      <div class="back">
+        <img src="https://docs.microsoft.com/fr-fr/dotnet/architecture/cloud-native/media/backend-for-frontend-pattern.png" alt="backend developement (Secure, scalable...)"/>
+        <h2>Back</h2>
+        <h3>Langages</h3>
+        <p>Java, PHP, TypeScript, SQL</p>
+        <h3>Frameworks</h3>
+        <p>Spring, Symfony, NodeJS/Express, Nest</p>
+        <h3>Autres outils / concepts</h3>
+        <p>JGiven, Hibernate, API, REST, Serverless, Mockito</p>
+      </div>
+      <div class="ops">
+        <img src="https://d33wubrfki0l68.cloudfront.net/2475489eaf20163ec0f54ddc1d92aa8d4c87c96b/e7c81/images/docs/components-of-kubernetes.svg" alt="autres compétences (agilité, créativité, ouverture...)"/>
+        <h2>Ops</h2>
+        <h3>Cloud provider</h3>
+        <p>Google cloud platform</p>
+        <h3>Outils de déploiement</h3>
+        <p>Kubernetes, Flux & Helm</p>
+        <h3>CI/CD</h3>
+        <p>Jenkins</p>
+      </div>
+    </header>
+    <footer>
       <img src="https://clubagileprovence.files.wordpress.com/2016/09/agile-mots.jpg?w=739" alt="autres compétences (agilité, créativité, ouverture...)"/>
-      <h2>Autres compétences</h2>
-      <h3>Craftsmanship</h3>
-      <p>BDD, DDD, TDD</p>
-      <h3>Agilité</h3>
-      <p>Connaissance de la philosophie, des valeurs et des principes, Scrum, Kanban</p>
-      <h3>Autres</h3>
-      <p>Formation, accompagnement, leadership et quelques bases UX/UI & Ops</p>
-    </div>
-  </div>
-</section>
+      <section>
+        <h2>Autres compétences</h2>
+        <h3>Craftsmanship</h3>
+        <p>BDD, DDD, TDD</p>
+        <h3>Agilité</h3>
+        <p>Connaissance de la philosophie, des valeurs et des principes, Scrum, Kanban</p>
+        <h3>Soft skills</h3>
+        <p>Formation, accompagnement au changement, leadership, esprit critique, créativité</p>
+      </section>
+    </footer>
+  </article>
+</article>
