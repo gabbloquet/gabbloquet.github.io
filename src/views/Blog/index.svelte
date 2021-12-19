b4c1c7e6a6ef11d182d6e5e8c202c70ffbeae5fe
Bloquet Gabin <gabin.bloquet@decathlon.com>
2021-12-19 15:21:52 +0100

    feat: back to portfolio

--- src/views/Blog/index.svelte
+++ src/views/Blog/index.svelte
@@ -26,6 +26,10 @@
     </ol>
   </nav>
 
+  <a class="portfolio-link" href="/" use:link>
+    <img src="/assets/img/portfolio.png" alt="blog" height="60px" />
+  </a>
+
   {#if articleToDisplay}
     <article class="article">
       <header>
