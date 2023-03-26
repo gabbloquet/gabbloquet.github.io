c80bd6ecdbc323efecd4091bbd69f5d306e7d782
Gabin Bloquet <gabin.blqt@gmail.com>
2023-03-26 16:45:28 +0200

    fix: faute d'orthographe

--- src/views/Home.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Header from '../containers/Header/Header.svelte'
-  import Content from '../containers/Content/Content.svelte'
-  import Footer from '../containers/Footer/Footer.svelte'
-</script>
-
-<Header />
-<Content />
-<Footer />
