b2479491be681d5b0d665a0ce19a61d626828eb0
Gabin BLOQUET <gabin.blqt@gmail.com>
2024-10-08 09:01:29 +0200

    chore: add tgm

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,26 @@
   import Home from './views/Home.svelte'
   import Blog from './views/Blog/index.svelte'
   import NotFound from './views/NotFound.svelte'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    // Créer l'élément noscript
+    const noscriptTag = document.createElement('noscript')
+
+    // Créer l'élément iframe à l'intérieur de noscript
+    const iframeTag = document.createElement('iframe')
+    iframeTag.src = 'https://www.googletagmanager.com/ns.html?id=GTM-WB8PBSXT'
+    iframeTag.height = '0'
+    iframeTag.width = '0'
+    iframeTag.style.display = 'none'
+    iframeTag.style.visibility = 'hidden'
+
+    // Ajouter l'iframe à l'intérieur du noscript
+    noscriptTag.appendChild(iframeTag)
+
+    // Ajouter l'élément noscript au body
+    document.body.appendChild(noscriptTag)
+  })
 
   const routes = {
     '/': Home,
